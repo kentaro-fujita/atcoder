9ddfe43f0a963ff6c775073f3ccd2deac49f91c8
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2020-04-28 16:41:35 +0900

    add practice problem

--- ABC157/D.py
+++ ABC157/D.py
@@ -1,16 +1,59 @@
-def main():
-    N, M, K = map(int, input().split())
+N, M, K = map(int, input().split())
+F = [[] for _ in range(N)]
+B = [[] for _ in range(N)]
 
-    AB = [list(map(int, input().split())) for _ in range(M)]
-    CD = [list(map(int, input().split())) for _ in range(K)]
+for i in range(M):
+    a, b = map(int, input().split())
+    F[a-1].append(b-1)
+    F[b-1].append(a-1)
 
-    for A, B in AB:
-        pass
+for i in range(K):
+    c, d = map(int, input().split())
+    B[c-1].append(d-1)
+    B[d-1].append(c-1)
 
-    for C, D in CD:
-        pass
 
-    
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [0] * n
+        self.size = [1] * n
 
-if __name__ == '__main__':
-    main()
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return
+        
+        if self.rank[x] < self.rank[y]:
+            self.par[x] = y
+            self.size[y] += self.size[x]
+        else:
+            self.par[y] = x
+            self.size[x] += self.size[y]
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+
+
+UF = UnionFind(N)
+for iam in range(N):
+    for friend in F[iam]:
+        UF.unite(iam, friend)
+
+ans = [UF.size[UF.find(iam)] - 1 for iam in range(N)]
+for iam in range(N):
+    ans[iam] -= len(F[iam])
+
+for iam in range(N):
+    for block in B[iam]:
+        if UF.find(iam) == UF.find(block):
+            ans[iam] -= 1
+
+print(*ans, sep=' ')
---
+++ ABC157/D2.py
@@ -0,0 +1,44 @@
+N, M, K = map(int, input().split())
+F = [[] for _ in range(N)]
+B = [[] for _ in range(N)]
+
+for i in range(M):
+    a, b = map(int, input().split())
+    F[a-1].append(b-1)
+    F[b-1].append(a-1)
+
+for i in range(K):
+    c, d = map(int, input().split())
+    B[c-1].append(d-1)
+    B[d-1].append(c-1)
+
+D = {}
+parent = [-1] * N
+visited = [False] * N
+for root in range(N):
+    if visited[root]:
+        continue
+
+    D[root] = set([root])
+    stack = [root]
+    while stack:
+        n = stack.pop()
+        visited[n] = True
+        parent[n] = root
+
+        for to in F[n]:
+            if visited[to]:
+                continue
+            D[root].add(to)
+            stack.append(to)
+    
+ans = [0] * N
+for iam in range(N):
+    group = D[parent[iam]]
+    tmp_ans = len(group) - len(F[iam]) - 1
+    for block in B[iam]:
+        if block in group:
+            tmp_ans -= 1
+    ans[iam] = tmp_ans
+
+print(*ans, sep=' ')
