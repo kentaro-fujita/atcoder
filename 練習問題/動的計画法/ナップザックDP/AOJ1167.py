8c510d18bce56fe4dd10b3418627ab9454e01a78
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2020-05-09 23:24:18 +0900

    add practice problem

---
+++ 練習問題/動的計画法/ナップザックDP/AOJ1167.py
@@ -0,0 +1,30 @@
+MAX = 10**6 + 1
+
+sq = []
+osq = []
+i = 0
+while (i+1) * (i+2) * (i+3) // 6 <= MAX:
+    sq.append((i+1) * (i+2) * (i+3) // 6)
+    if sq[i] % 2 == 0:
+        osq.append(sq[i])
+    i += 1
+
+dp = [float("inf")]* MAX
+dp[0] = 0
+for i in range(MAX):
+    for j in range(len(sq)):
+        if i + sq[j] >= MAX: break
+        dp[i+sq[j]] = min(dp[i+sq[j]], dp[i]+1)
+
+odp = [float("inf")]* MAX
+odp[0] = 0
+for i in range(MAX):
+    for j in range(len(osq)):
+        if i + osq[j] >= MAX: break
+        odp[i+osq[j]] = min(dp[i+osq[j], odp[i]+1])
+
+while True:
+    n = int(input())
+    if n == 0: break
+
+    print(f"{dp[n]} {odp[n]}")
