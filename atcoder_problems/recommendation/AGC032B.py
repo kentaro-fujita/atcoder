01351e6895d736a94708c02e2397dbdad9ee7b1e
Kentaro Fujita <50708397+kentaro-fujita@users.noreply.github.com>
2020-06-24 11:59:41 +0900

    add practice problem

--- atcoder_problems/recommendation/AGC032B.py
+++ atcoder_problems/recommendation/AGC032B.py
@@ -1,2 +1,14 @@
 n = int(input())
+d = n
+ans = []
+if n % 2 == 1: d -= 1
 
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if j <= i or j == d: continue
+        ans.append([i, j])
+    d -= 1
+
+print(len(ans))
+for f, s in ans:
+    print(f, s)
